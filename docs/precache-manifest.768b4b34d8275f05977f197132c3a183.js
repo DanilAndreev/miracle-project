b16f42a42e32e4be845c1fc635ec5db491be8076
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-09-10 17:18:52 +0300

    Added docs

---
+++ docs/precache-manifest.768b4b34d8275f05977f197132c3a183.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb948f7efce5c8cc4abec3bc9875941",
+    "url": "/miracle-project/index.html"
+  },
+  {
+    "revision": "25bbf078af2799e29de2",
+    "url": "/miracle-project/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "47e206e3d1f6c0deccbf",
+    "url": "/miracle-project/static/js/2.33994003.chunk.js"
+  },
+  {
+    "revision": "35a08d2e06945b779fadacb826071be3",
+    "url": "/miracle-project/static/js/2.33994003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25bbf078af2799e29de2",
+    "url": "/miracle-project/static/js/main.ed064faf.chunk.js"
+  },
+  {
+    "revision": "2cd7de9d4ebf072b3824",
+    "url": "/miracle-project/static/js/runtime-main.ed802f05.js"
+  },
+  {
+    "revision": "d788840ca5a5e2d60df8dcb625f332ce",
+    "url": "/miracle-project/static/media/HES.d788840c.jpg"
+  }
+]);
